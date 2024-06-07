598585ca8cd05fe985b2ac1da7d8ebf2e2573ba3
bardoquinq2169 <159388277+bardoquinq2169@users.noreply.github.com>
2024-06-07 14:54:38 +1200

    complete integer checker

---
+++ C_02_BFQ_intcheck.py
@@ -0,0 +1,52 @@
+# checks for an integer with optimal upper /
+# lower limits and an optional exit code for infinite mode
+# / quiting the game
+def int_check(question, low=None, high=None, exit_code=None):
+    # if any integer is allowed...
+    if low is None and high is None:
+        error = "Please enter an integer"
+
+    # if the number needs to be more than an
+    # integer (ie: rounds / 'high number')
+    elif low is not None and high is None:
+        error = (f"Please enter an integer that is "
+                 f"more than / equal to {low}")
+
+    # if the number needs to be between low and high
+    else:
+        error = (f"Please enter an integer that"
+                 f" is between {low} and {high} (inclusive)")
+
+    while True:
+        response = input(question).lower()
+
+        # check for infinite mode / exit code
+        if response == exit_code:
+            return response
+
+        try:
+            response = int(response)
+
+            # Check the integer is not too low...
+            if low is not None and response < low:
+                print(error)
+
+            # check response is more than the low number
+            elif high is not None and response > high:
+                print(error)
+
+            # if the response is valid, return it
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# Ask user for number of rounds / infinite mode
+num_questions = int_check("How many questions would you like? Push <enter> for infinite mode>: ",
+                          low=1, exit_code="")
+
+if num_questions == "":
+    mode = "infinite"
+    num_questions = 5
